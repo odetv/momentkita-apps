37a5a5a07cc10e1f00422d8c725d6642d971eaed
odetv <abdiutama02@gmail.com>
2024-01-13 20:24:42 +0800

    testing nextui

--- apps/landing-page/src/app/layout.tsx
+++ apps/landing-page/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "MOMENTKITA BALI",
@@ -20,7 +21,9 @@ export default function RootLayout({
         />
       </head>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Providers>{children}</Providers>
+        </body>
       </html>
     </>
   );
--- apps/landing-page/src/app/page.tsx
+++ apps/landing-page/src/app/page.tsx
@@ -1,8 +1,30 @@
+import {
+  Button,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
+
 export default function Home() {
   return (
     <>
+      <Button>Click me</Button>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="bordered">Open Menu</Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          <DropdownItem key="new">New file</DropdownItem>
+          <DropdownItem key="copy">Copy link</DropdownItem>
+          <DropdownItem key="edit">Edit file</DropdownItem>
+          <DropdownItem key="delete" className="text-danger" color="danger">
+            Delete file
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
       <h1 className="flex justify-center items-center min-h-screen w-full text-center text-3xl font-bold">
-        Welcome to Momentkita Bali Apps!
+        Welcome to Apps!
       </h1>
     </>
   );
